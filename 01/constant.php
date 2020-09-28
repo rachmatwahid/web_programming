a5e6fc211c6aaa99a841f73a19612c477e27034f
rachmatwahid <32413014+rachmatwahid@users.noreply.github.com>
2020-09-28 13:54:26 +0700

    initial commit

---
+++ 01/constant.php
@@ -0,0 +1,20 @@
+<?php 
+
+// Constant can NOT be changed
+define('phi', 3.14);
+
+// Uncomment this to see why
+// phi = phi + 1;
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Constant</title>
+ </head>
+ <body>
+ 	<!-- Access constant without $ -->
+ 	<p><?php echo phi; ?></p>
+ </body>
+ </html>
---
+++ 01/standard_output.php
@@ -0,0 +1,16 @@
+<?php 
+$nama = 'Adi';
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Standard Output</title>
+ </head>
+ <body>
+ 	<!-- Gunakan function echo, print, atau var_dump -->
+ 	<p><?php echo $nama; ?></p>
+ 	<p><?php print($nama); ?></p>
+ 	<p><?php var_dump($nama); ?></p>
+ </body>
+ </html>
