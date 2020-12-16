125308adf720794c616cf3925bace76b34e237d9
rachmatwahid <32413014+rachmatwahid@users.noreply.github.com>
2020-12-16 10:32:50 +0700

    initial commit

---
+++ geotag_tourism/map.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+	<title>Find Your Pets</title>
+
+	<link rel="stylesheet" href="lib/leaflet/leaflet.css" />
+	<link rel="stylesheet" href="lib/cluster/MarkerCluster.css" />
+	<link rel="stylesheet" href="Leaflet.Photo.css" />	
+	<link rel="stylesheet" href="map.css" />
+
+</head>
+
+<body>
+
+	<div id="map"></div>
+
+	<script src="lib/reqwest.min.js"></script>
+	<script src="lib/leaflet/leaflet.js"></script>
+	<script src="lib/cluster/leaflet.markercluster.js"></script>
+	<script src="Leaflet.Photo.js"></script>
+	<script src="map.js"></script>
+
+</body>
+</html>
